2803b0c4a298ac3e8f72c06eb05a3b8882d01c11
takaHAL <takaonoyuki0610@gmail.com>
2021-02-13 16:19:13 +0900

    :fire: 不要な記述の削除

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,6 @@ import { terser } from 'rollup-plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-css-only';
-import sveltePreprocess from 'svelte-preprocess';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -42,7 +41,6 @@ export default {
 	plugins: [
 		svelte({
 			preprocess: sveltePreprocess({ postcss: true }),
-			preprocess: sveltePreprocess(),
 			compilerOptions: {
 				// enable run-time checks when not in production
 				dev: !production
