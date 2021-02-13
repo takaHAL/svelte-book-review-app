f4bb01cca28e9cbb3bbb77a0728a1ed83d16a966
takaHAL <takaonoyuki0610@gmail.com>
2021-02-13 17:13:18 +0900

    :sparkles: 本検索機能追加

--- src/pages/SearchBook.svelte
+++ src/pages/SearchBook.svelte
@@ -1,13 +1,52 @@
 <script lang="ts">
   import SearchBar from '../components/SearchBar.svelte'
+  import Spinner from '../components/Spinner.svelte'
+  import BookCard from '../components/BookCard.svelte'
+  import type { BookItem, Result } from '../repositories/book'
+  import RepositoryFactory, { BOOK } from '../repositories/RepositoryFactory'
+  const BookRepository = RepositoryFactory[BOOK]
 
   let q = 'JavaScript'
+  let empty = false
+  let books: BookItem[] = []
+  let promise: Promise<void>
+
+  const handleSubmit = () => {
+    if (!q.trim()) return
+    promise = getBooks()
+  }
+
+  const getBooks = async () => {
+    books = []
+    empty = false
+    const result = await BookRepository.get({ q })
+    empty = result.totalItems === 0
+    books = result.items
+  }
 </script>
 
-<form>
+<!-- event.preventDefault()が実行される前に呼びだす -->
+<form on:submit|preventDefault={handleSubmit}>
   <SearchBar bind:value={q} />
 </form>
 
 <div class="text-center mt-4">
-  { q }
+  {#if empty}
+    <div>検索結果が見つかりませんでした。</div>
+  {:else}
+    <div class="grid grid-cols-1 gap-2 lg:grid-cols-2">
+      {#each books as book (book.id)}
+        <BookCard {book} />
+      {/each}
+    </div>
+  {/if}
+  {#await promise}
+    <div class="flex justify-center">
+      <Spinner />
+    </div>
+    {:catch e}
+      <span class="text-red-600 text-sm">
+        {e.message}
+      </span>
+  {/await}
 </div>
